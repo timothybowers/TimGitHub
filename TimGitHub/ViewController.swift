b29a445abbbe4180055dfc648ba65838ec401253
Tim <timbigdev@gmail.com>
2019-09-03 18:06:47 +1000

    Initial Commit

---
+++ TimGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimGitHub
+//
+//  Created by Timothy on 3/9/19.
+//  Copyright © 2019 Timothy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
